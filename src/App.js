204aa2ea5d324c4d1acae7aacc55a3f9ecf7987a
Cristian Perez <cristian.perez.domper@gmail.com>
2024-07-19 09:14:01 -0500

    Agrega componentes y el uso de hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
+import React, {useState} from 'react';
 import './App.css';
 import Header from './componentes/Header';
+import FormularioTareas from './componentes/FormularioTareas';
+import ListaTareas from './componentes/ListaTareas';
 
 const App = () => {
-    return (
-        <div className="contenedor">
-            <Header />
-        </div>
-    );
+	const [tareas, cambiarTareas] = useState([
+		{
+			id: 1,
+			texto: 'Lavar la ropa',
+			completada: false,
+		},
+		{
+			id: 2,
+			texto: 'Verificar tutorial',
+			completada: false,
+		},
+	]);
+
+	console.log(tareas);
+
+	return (
+		<div className="contenedor">
+			<Header />
+			<FormularioTareas
+				tareas={tareas}
+				cambiarTareas={cambiarTareas}
+			/>
+			<ListaTareas
+				tareas={tareas}
+				cambiarTareas={cambiarTareas}
+			/>
+		</div>
+	);
 };
 
 export default App;
